c30cd26bf9648b3ef03634b65404dcb44be6b3b1
Nomansky47 <kita31200@gmail.com>
2023-04-17 21:10:25 +0300

    update

---
+++ ДЗ Serialization/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Serial;
+
+namespace ConsoleApp2
+{
+
+    internal class Program
+    {
+       
+        static void Main()
+        {
+            Guy guy = new Guy();
+            guy.Name = "Andrey";
+            guy.Number = 10;
+            string des = guy.MySerializer<Guy>();
+            Console.WriteLine(des);
+            Guy serguy = new Guy();
+            serguy = des.MyDeserializer<Guy>();
+            Console.WriteLine(serguy.Number+" "+ serguy.Name);
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ ДЗ Serialization/Serial/Deserial.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace Serial
+{
+    public static class Deserial
+    {
+        public static T MyDeserializer<T>(this string obj)
+        {
+
+            return JsonSerializer.Deserialize<T>(obj);
+        }
+    }
+}
